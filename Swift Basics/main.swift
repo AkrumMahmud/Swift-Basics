7eda72172054a9285609c19940983acd7d89d3b0
AkrumMahmud <kekezie89@gmail.com>
2018-08-27 17:59:01 -0500

    First commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,96 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Akrum Mahmud on 8/26/18.
+//  Copyright © 2018 Akrum Mahmud. All rights reserved.
+//
+
+import Foundation
+
+//Variables
+let sample1: UInt8 = 0x3A
+var sample2: UInt8 = 58
+var heartRate = 85
+var deposits: Double = 125002796
+let acceleration: Float = 9.800
+var mass: Float = 14.6
+var distance = 129.763001
+var lost = true
+var expensive = true
+var choice = 2
+let integral: Character = "\u{222B}"
+let greeting = "Hello"
+var name = "Karen"
+
+//Some code
+if(sample1 == sample2){
+    print("The samples are equal.")
+}
+else{
+    print("The samples are not equal.")
+}
+
+if(heartRate >= 40 && heartRate <= 80){
+    print("Heart rate is normal.")
+}
+else{
+    print("Heart rate is not normal.")
+}
+
+if(deposits >= 100000000){
+    print("You are exceedingly wealthy.")
+}
+else{
+    print("Sorry, you are broke as a joke")
+}
+
+//Another variable
+var force = mass * acceleration
+
+//More code
+print("force = \(force)")
+
+print("\(distance) is the distance")
+
+if(lost == true && expensive == true){
+    print("I am really sorry! I will get the manager.")
+}
+
+if(lost == true && expensive == false){
+    print("Here is a coupon for 10% off")
+}
+
+//Switch Case
+switch choice{
+    
+    case 1:
+        print("You chose 1.")
+    
+    case 2:
+        print("You chose 2.")
+    
+    case 3:
+        print("You chose 3.")
+    
+    default:
+        print("You made an unkown choice.")
+}
+
+print("\(integral) is a string")
+
+//For loop iteration
+for i in 5...10{
+    print("i = \(i)")
+}
+
+//Last variable declaration
+var age = 0
+
+//While loop
+while(age < 6){
+    print("age = \(age)")
+    age += 1
+}
+
+print("\(greeting) \(name)")
